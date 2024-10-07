efa5d309cd969fd4355f3df0e6d34334f370dd3b
MatiasMatikainen <matias.mm115@gmail.com>
2024-10-07 14:18:03 +0300

    toimiva filter lisätty

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import './Expenses.css';
 
 const Expenses = (props) => {
 
-    const [filteredYear, setFilteredYear] = useState('2024');
+    const [filteredYear, setFilteredYear] = useState('2023');
         
     const filterChangeHandler = (selectedYear) => {    
         //console.log('Expenses.js');
@@ -14,29 +14,28 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+    
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
+            <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
             />
-            <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-            />
-            <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-            />
-            <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-            />
+            {filteredExpenses-length === 0 ? (
+                <p>No expenses found.</p>
+            ) : (
+                filteredExpenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            )}
         </Card>
     );
 }
